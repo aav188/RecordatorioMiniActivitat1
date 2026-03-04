9150c74a2d17b045073a238ad250acde0d800ebe
aav18 <aav18@alumnes.udl.cat>
2026-03-04 22:32:10 +0100

    Como comenzar a usar compose

--- app/src/main/java/com/example/recordatoriominiactivitat1/MainActivity.kt
+++ app/src/main/java/com/example/recordatoriominiactivitat1/MainActivity.kt
@@ -38,7 +38,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, name = "Text Preview")
 @Composable
 fun GreetingPreview() {
     RecordatorioMiniActivitat1Theme {
